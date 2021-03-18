c07cc662616f0278489d1397d031402615fdc236
Wenxuan Zhang <wenxuangm@gmail.com>
2021-03-18 20:21:13 +0800

    bump up version

--- Formula/clitrans.rb
+++ Formula/clitrans.rb
@@ -4,10 +4,10 @@
 class Clitrans < Formula
   desc "Yet another command-line translator (Chinese <=> English)"
   homepage "https://github.com/wfxr/clitrans"
-  url "https://github.com/wfxr/clitrans/releases/download/v0.3.3/clitrans-v0.3.3-x86_64-apple-darwin.tar.gz"
-  sha256 "80070fc0cc1a12208f5472b9d16c5560847909773301628a4a01cf8c810942ee"
+  url "https://github.com/wfxr/clitrans/releases/download/v0.3.4/clitrans-v0.3.4-x86_64-apple-darwin.tar.gz"
+  sha256 "c09e4241e502b2ad6e23785b12c629c853433e36fee85b0c70402c2022c3c717"
   license "MIT/APACHE-2.0"
-  version "0.3.3"
+  version "0.3.4"
 
   def install
     bin.install "clitrans"
